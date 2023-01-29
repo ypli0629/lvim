4d306d5359f35bd9e261518e30d6c70eb2ae6eaa
ypli0629 <liyapeng0629@gmail.com>
2023-01-29 16:59:16 +0800

    feat: use catppuccin

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,9 +1,10 @@
-lvim.colorscheme = "darkplus"
+lvim.colorscheme = "catppuccin"
+lvim.transparent_window = true
 lvim.log.level = "warn"
 lvim.lsp.diagnostics.float.focusable = true
 lvim.format_on_save = {
-  enabled = true,
-  timeout = 10000
+	enabled = true,
+	timeout = 10000,
 }
 lvim.builtin.dap.active = false
 lvim.builtin.nvimtree.setup.filters.dotfiles = true
